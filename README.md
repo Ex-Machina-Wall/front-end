f6f3eb9ece7da095028e997a0d1a1733d7c5ede4
Trevor O'Leary <accounts@trevoroleary.com>
2024-07-21 18:51:30 -0700

    update endpoint to be http instead of mqtt

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+# Hostname of pi must be 'four'
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/endpoints.js
+++ src/endpoints.js
@@ -1,13 +1,12 @@
 // math.js
 export function send_post(body, timeOfLastSend) {
   if (Date.now() - timeOfLastSend > 25){
-      fetch("http://nuc-homeserver:81/x",{
-        method: "POST",
-        body: body,
-        headers: {
-            "Authorization": `Bearer tk_mfzjlla9otq9ipggd0u0w490abu9i`
-        }
-      })
+    let formData = new FormData();
+    formData.append('instruction', body);
+    fetch("http://four:8000/instruction",{
+      method: "POST",
+      body: formData,
+    })
       return Date.now()
   }
   return timeOfLastSend
